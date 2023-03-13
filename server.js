1999bae9adae2c342319487122162eec3a425d0a
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2023-03-13 08:21:59 +0200

    last changes

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+
+require('dotenv').config();
+const app = express();
+
+const mongoUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
+mongoose.connect(mongoUrl);
+
+//parsing 
+app.use(bodyParser.json());
+
+//Sanitize
+app.use(xss());
+app.use(mongoSanitize());
+
+
+const port = process.env.PORT || 3001;
+app.listen(port, ()=> {
+    console.log('Server listen on port 3001')
+});
