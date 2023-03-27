9855eba351f8b6187028ef0413788c8ba0da4b13
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2023-03-27 11:41:28 +0300

    changes

--- routes/articles.route.js
+++ routes/articles.route.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const articlesController = require('../controllers/articles.controller');
+const auth = require('../middleware/auth');
+
+///               rolle  and recources
+router.post('/', auth('createAny', 'Articles'));
 
 
 module.exports = router;
