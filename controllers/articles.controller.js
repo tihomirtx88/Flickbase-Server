f05a421f4751b5e1740cb324c26d05d0b5e4f57f
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2023-03-27 14:53:07 +0300

    Create logic for get aricle by id with token and also like quest with no token

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -11,6 +11,24 @@ const articlesController = {
            next(error);
        }
     },
+    async getArticleById(req, res, next){
+        try {
+            const _id = req.params.id;
+            const article = await articleService.getArticleById(_id, req.user);
+            res.json(article);
+        } catch (error) {
+            next(error);
+        }
+    },
+    async getUserArticleById(req, res, next){
+        try {
+            const _id = req.params.id;
+            const article = await articleService.getUserArticleById(_id);
+            res.json(article);
+        } catch (error) {
+            next(error);
+        }
+    },
 };
 
 module.exports = articlesController;
--- services/articles.service.js
+++ services/articles.service.js
@@ -16,6 +16,34 @@ const addArticle = async (body) => {
     }
 };
 
+const getArticleById = async (_id, user) => {
+    try {
+        const article = await Article.findById(_id);
+        if(!article) throw new ApiError(httpStatus.NOT_FOUND, 'Article not found');
+        if (user.role === 'user' && article.status === 'draft') {
+            throw new ApiError(httpStatus.NOT_FOUND, 'Sorry you are not allowed');
+        }
+        return article;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const getUserArticleById = async (_id) => {
+    try {
+        const article = await Article.findById(_id);
+        if(!article) throw new ApiError(httpStatus.NOT_FOUND, 'Article not found');
+        if (article.status === 'draft') {
+            throw new ApiError(httpStatus.NOT_FOUND, 'Sorry you are not allowed');
+        }
+        return article;
+    } catch (error) {
+        throw error;
+    }
+};
+
 module.exports = {
     addArticle,
+    getArticleById,
+    getUserArticleById
 };
